83e1620f23e805f9f8d581c50336c374ffd5d669
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-14 20:46:26 +0100

    integrate about-component into main

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
