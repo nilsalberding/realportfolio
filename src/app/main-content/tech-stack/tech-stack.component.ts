a6fd4918a36a4e607fb90118adc9787a4dc47ae2
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-17 14:11:13 +0100

    add tech-stack array

--- src/app/main-content/tech-stack/tech-stack.component.ts
+++ src/app/main-content/tech-stack/tech-stack.component.ts
@@ -8,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class TechStackComponent {
 
+  icons = [
+    {
+      path: 'assets/icons/angular.svg',
+      lable: 'Angular'
+    },
+    {
+      path: 'assets/icons/typescript.svg',
+      lable: 'TypeScript'
+    },
+    {
+      path: 'assets/icons/javascript.svg',
+      lable: 'JavaScript'
+    },
+    {
+      path: 'assets/icons/html.svg',
+      lable: 'HTML'
+    },
+    {
+      path: 'assets/icons/css.svg',
+      lable: 'CSS'
+    },
+    {
+      path: 'assets/icons/tailwind.svg',
+      lable: 'Tailwind'
+    },
+    {
+      path: 'assets/icons/api.svg',
+      lable: 'REST-API'
+    },
+    {
+      path: 'assets/icons/firebase.svg',
+      lable: 'Firebase'
+    },
+    {
+      path: 'assets/icons/git.svg',
+      lable: 'Git'
+    },
+    {
+      path: 'assets/icons/jira.svg',
+      lable: 'Jira'
+    },
+  ];
+
 }
