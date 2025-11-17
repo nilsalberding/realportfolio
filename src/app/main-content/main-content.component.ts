190d68bf00cea9a3f66e3f27baba24c842c41287
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-17 14:10:37 +0100

    build html structure for tech-component and intergrate in main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, TechStackComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
