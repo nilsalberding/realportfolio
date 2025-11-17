22aeef771fa297d12240692f71407fd8b71d3c18
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-17 15:01:10 +0100

    add html-structure to project-section and integrate into main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, TechStackComponent],
+  imports: [HeroComponent, AboutComponent, TechStackComponent, ProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
