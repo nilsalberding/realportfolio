0e47952eb47c12620dc3054288440d2a90a7712b
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-20 10:16:46 +0100

    create html-structure for contact-section and integrate into main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { TechStackComponent } from './tech-stack/tech-stack.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, TechStackComponent, ProjectsComponent],
+  imports: [HeroComponent, AboutComponent, TechStackComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
